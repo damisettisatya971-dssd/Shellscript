4b85571f49cb3684c085a16b87011fa557391483
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 12:33:48 +0530

    Hello world script

---
+++ 01-HelloWorld.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hi This is Satya, practicing shell scripting."
+echo " Hello this is Ramya, good to know about that. I'm also interested to learn"
+echo "oh!! that's great Ramya, I would like to explain if you are fine"
+echo "Thank you so much Satya, lets start the sessions today itself."
