cc256698458a89520fda7f7cca851b49dcd57dc2
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 16:54:15 +0530

    variables practice

--- 05-envvaribales.sh
+++ 05-envvaribales.sh
@@ -4,3 +4,5 @@ echo "Course name is $course"
 #If we want to set any variable with fixed value we can defien that in .bashrc file as below
 #export course="DevSecOps with AWS"
 #then run source .bashrc
+#if we give two values to the same env varibale, the one which is running in the session takes the precedence
+#if there is no value set in the current sessiom then env varibale defined in the .bashrc will exceutes
