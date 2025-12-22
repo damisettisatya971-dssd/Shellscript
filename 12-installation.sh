d1b35c717bfdc9dcf6664edda9a44e3621fa8a98
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 20:00:24 +0530

    shell practice

--- 12-installation.sh
+++ 12-installation.sh
@@ -3,7 +3,7 @@ userid=$(id -u)
 if [ $userid -ne 0 ]; then
  echo "Ensure you have admin access to install mysql"
  exit 1
-fi
+
 dnf install mysql -y
 if [ $? -ne 0 ]; then
   echo "ERROR: MySQL installation got failed"
