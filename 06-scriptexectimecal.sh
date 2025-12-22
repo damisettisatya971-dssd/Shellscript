306ec4645ab55d590dbf27f1db3041696d9d78d3
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 17:00:02 +0530

    variables practice

--- 06-scriptexectimecal.sh
+++ 06-scriptexectimecal.sh
@@ -3,5 +3,5 @@ Date=$(date)
 start_time=$(date +%s)
 sleep 10
 end_time=$(date +%s)
-totaltime=$($start_time-$end_time)
+totaltime=$(($start_time-$end_time))
 echo "Total script execution time is : $totaltime"
