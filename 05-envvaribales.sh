e41085913d40fd0bfee97805b8078c3d6ed9e174
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 16:49:33 +0530

    variables practice

--- 05-envvaribales.sh
+++ 05-envvaribales.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "Course name is $course" 
+#Environment variable is valid only till the process is running.
+#If we want to set any variable with fixed value we can defien that in .bashrc file as below
+#export course="DevSecOps with AWS"
+#then run source .bashrc
