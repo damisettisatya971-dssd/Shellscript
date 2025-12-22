93c84f0f1ee2b29592f9820e1a2309c5693ede9e
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 17:50:25 +0530

    variables practice

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -2,7 +2,7 @@
 n1=145
 n2=55
 F3="Priyanka"
-Flowers = ("Jasmine" "Lilly" "Rose")
+Flowers=("Jasmine" "Lilly" "Rose")
 sum=$n1+$n2
 echo "Sum of the nubers is $sum"
 echo "The first flower is $(Flowers[0])"
