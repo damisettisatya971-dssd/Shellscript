6960ad02ebe6fa499a2d2dfc0ab79e09a586c598
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 16:24:53 +0530

    variables practice

---
+++ 04-Readingvariables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo"Please enter your name:"
+read -s Name
+echo"Hi $Name, please enter your phone number :"
+read -s PH_Number
+echo"OTP has been sent to your $PH_Number, please enter :"
+read -s OTP
+echo"Entered OTP is correct"
