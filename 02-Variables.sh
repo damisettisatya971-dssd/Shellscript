67ce45be9a8810b2da47566e9d19c9e9e94cf37b
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 12:37:32 +0530

    variables practice

---
+++ 02-Variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+$Name1=Satya
+$Name2=Ramya
+echo "Hi This is $Name1, practicing shell scripting."
+echo " Hello this is $Name2, good to know about that. I'm also interested to learn"
+echo "oh!! that's great $Name2, I would like to explain if you are fine"
+echo "Thank you so much $Name1, lets start the sessions today itself."
