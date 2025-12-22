f5127e9927b3734f6e2d9d764c6db9b379b7128c
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 18:01:10 +0530

    variables practice

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -2,10 +2,10 @@
 n1=145
 n2=55
 F3="Priyanka"
-Flowers=("Jasmine" "Lilly" "Rose")
+Flowers=("Jasmine" "Lilly" "Rose") #size=3, max index=2 index starts with 0
 sum=$(($n1+$n2))
 echo "Sum of the nubers is $sum"
 echo "The first flower is ${Flowers[0]}"
 echo "All flowers are ${Flowers[@]}"
 echo "The second flower is ${Flowers[1]}"
-echo "The third flower is ${Flowers[20]}"
+echo "The third flower is ${Flowers[2]}"
---
+++ 09-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Enter a numer to check:"
+read NUM
+if[$NUM -lt 10];then
+ echo "$NUM is less than 10"
+elif$NUM -eq 10];then
+ echo "$NUM is equals to 10"
+else
+ echo "$NUM is greater than 10"
+fi 
+# -gt greater than
+# -eq equals to
+# -ne not equal
+
