8320eb6c6d14d89f46891daba5fef96155846355
damisettisatya971-dssd <damisettisatya971@gmail.com>
2025-12-22 18:21:54 +0530

    variables practice

--- 10-even or odd.sh
+++ 10-even or odd.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Enter a number:"
 read NUM
-if [ $(($NUM % 2)) -eq 0 ]; then
+if [ $NUM -eq 0]; then
+ echo "Entered number $NUM is 0, which is neither odd nor even"
+elif [ $(($NUM % 2)) -eq 0 ]; then
  echo "$NUM is even number"
 else
   echo "$NUM is odd number"
