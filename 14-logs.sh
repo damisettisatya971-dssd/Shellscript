68379e5ccdaf4fc4a5116b1408c6fa489f3c8a88
damisettisatya971-dssd <damisettisatya971@gmail.com>
2026-01-29 10:20:04 +0530

    Shell practice

--- 14-logs.sh
+++ 14-logs.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0|cut -d "." -f1)
-LOG_FILE="$(LOGS_FOLDER)/$(SCRIPT_NAME).log" # /var/log/shell-script/14-logs.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/14-logs.log
 
 mkdir -p $LOGS_FOLDER
 
